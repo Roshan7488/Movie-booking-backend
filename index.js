401c0d09fa275e7dc236f345f318b6dae900e314
Roshan <roshangupta74888121@gmail.com>
2026-09-14 11:20:09 +0530

    Feature:Basic Express app with mongoose setup done

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const env = require('dotenv');
+const mongoose = require('mongoose');
+
+
+env.config();
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  return res.json({ message: 'Hello, World!' });
+});
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on http://localhost:${process.env.PORT}`);
+  mongoose.connect(process.env.DB_URL).then(()=>{console.log("Successfully connected to mongoose");
+  }).catch((err)=>{
+    console.error("Error connecting to mongoose:", err);
+  });
+  
+});
