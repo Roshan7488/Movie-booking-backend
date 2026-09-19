12c5d70c870656268388716a3efe07a5c3236687
Roshan <roshangupta74888121@gmail.com>
2026-09-19 14:14:27 +0530

    Feat:Added get theatre api

--- routes/theatre.routes.js
+++ routes/theatre.routes.js
@@ -1,7 +1,11 @@
 const theatreController=require('../controllers/theatre.controller');
+const theatreMiddleware=require('../middlewares/theatre.middleware');
 
 const routes=(app)=>{
-  app.post('/mba/api/v1/theatres',theatreController.create);
+  app.post('/mba/api/v1/theatres',theatreMiddleware.validateTheatreCreateRequest,theatreController.create);
+
+  app.get('/mba/api/v1/theatres/:id',theatreController.getTheatre);
+
 }
 
 module.exports=routes;
--- services/theatre.service.js
+++ services/theatre.service.js
@@ -1,24 +1,40 @@
-const Theatre= require('../models/theatre.model');
+const Theatre = require("../models/theatre.model");
 
-const createTheatre= async (data)=>{
-  try{
-  const response = await Theatre.create(data);
-  return response;
-  }catch(error){
-    if(error.name==='ValidationError'){
-      let err={};
-    Object.keys(error.errors).forEach((key)=>{
-      err[key]=error.errors[key].message;
-    });
-    console.log(err);
-    return {err:err,code:422};
-    }else{
+const createTheatre = async (data) => {
+  try {
+    const response = await Theatre.create(data);
+    return response;
+  } catch (error) {
+    if (error.name === "ValidationError") {
+      let err = {};
+      Object.keys(error.errors).forEach((key) => {
+        err[key] = error.errors[key].message;
+      });
+      console.log(err);
+      return { err: err, code: 422 };
+    } else {
       throw err;
     }
   }
-  
-}
+};
 
-module.exports={
-  createTheatre
-}
+const getTheatre = async (id) => {
+  try {
+    const response = await Theatre.findById(id);
+    if (!response) {
+      return {
+        err: "No theatre found for the given id",
+        code: 404,
+      };
+    }
+    return response;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+module.exports = {
+  createTheatre,
+  getTheatre
+};
