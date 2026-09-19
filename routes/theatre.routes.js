29c73648b2248c0d49e5c75c6c27b3775a1797f8
Roshan <roshangupta74888121@gmail.com>
2026-09-19 15:19:23 +0530

    Feat:Implemented the delete api for theatre resource

--- routes/theatre.routes.js
+++ routes/theatre.routes.js
@@ -8,6 +8,8 @@ const routes=(app)=>{
 
   app.get('/mba/api/v1/theatres',theatreController.getTheatres);
 
+  app.delete('/mba/api/v1/theatres/:id',theatreController.destroy);
+
 }
 
 module.exports=routes;
--- services/theatre.service.js
+++ services/theatre.service.js
@@ -55,8 +55,25 @@ catch(error){
 }
 }
 
+const deleteTheatre= async (id)=>{
+  try{
+    const response= await Theatre.findByIdAndDelete(id);
+    if(!response){
+      return {
+        err:"No record of a theatre found for the given id",
+        code:404
+      }
+    }
+    return response;
+  }catch(error){
+    console.log(error);
+    throw error;
+  }
+}
+
 module.exports = {
   createTheatre,
   getTheatre,
-  getAllTheatre
+  getAllTheatre,
+  deleteTheatre
 };
