a2a08a88486020e00001d8e1b0d0d3401608cd99
Roshan <roshangupta74888121@gmail.com>
2026-09-26 22:58:46 +0530

    Feat: Added basic middlewares to auth

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,7 +1,8 @@
 const authController =require('../controllers/auth.controller');
+const authMiddleware=require('../middlewares/auth.middlewares');
 
 const routes = (app) =>{
-  app.post('/mba/api/v1/auth/signup',authController.signup);
+  app.post('/mba/api/v1/auth/signup',authMiddleware.validateSignupRequest,authController.signup);
 }
 
 module.exports=routes;
